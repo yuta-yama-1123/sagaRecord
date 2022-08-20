337bcb217225ce210d5d3664ddaaeeb13892dbc5
yuta-yama-1123 <yutaforios@gmail.com>
2022-08-20 18:54:03 +0900

    Initial Commit

---
+++ sagaRecord/sagaRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  sagaRecordApp.swift
+//  sagaRecord
+//
+//  Created by 山本祐太 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct sagaRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
